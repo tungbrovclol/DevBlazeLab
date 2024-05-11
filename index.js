7b3a0211002c4120ff55e65608b4535ae8d9ac64
tungbrovclol <bashar0907pun0suy@hotmail.com>
2024-05-11 17:27:56 +0700

    aptos commit

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-function averageArray(arr) {
-  return arr.reduce((acc, curr) => acc + curr, 0) / arr.length;
+function firstMissingPositive(nums) {
+  const n = nums.length;
+  for (let i = 0; i < n; i++) {
+    while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i]) {
+      const temp = nums[nums[i] - 1];
+      nums[nums[i] - 1] = nums[i];
+      nums[i] = temp;
+    }
+  }
+  for (let i = 0; i < n; i++) {
+    if (nums[i] !== i + 1) return i + 1;
+  }
+  return n + 1;
 }
