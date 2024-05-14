5aee56e25de8d073fafc72aad0345b7e91a9628c
tungbrovclol <bashar0907pun0suy@hotmail.com>
2024-05-14 14:09:28 +0700

    demo task

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const compact = (arr) => arr.filter(Boolean);
+function productExceptSelf(nums) {
+  const result = [];
+  let product = 1;
+  for (let i = 0; i < nums.length; i++) {
+    result[i] = product;
+    product *= nums[i];
+  }
+  product = 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    result[i] *= product;
+    product *= nums[i];
+  }
+  return result;
+}
